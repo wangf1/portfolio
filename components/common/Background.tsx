06da03176dab7a3e239646efed2be094c86453e6
Mark Wang <armstrong.wang2000@gmail.com>
2024-07-15 00:07:33 -0600

    Make background image looks better

--- components/common/Background.tsx
+++ components/common/Background.tsx
@@ -5,18 +5,19 @@ import Image from "next/image";
 const Background = () => {
   return (
     <div className="absolute inset-0 overflow-hidden">
-      {/* Centered Background Image */}
       <Image
         src={BackgroundImage}
         alt="Background Image"
-        className="rotate-[15deg] opacity-50"
+        className="absolute inset-0 m-auto rotate-[15deg] 
+          opacity-50 z-0 blur-[2px]"
       />
-
       <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="gray" />
+      {/* Radial gradient for the container to give a faded look */}
       <div
-        className="h-screen w-full bg-background 
-        dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex
-        items-center justify-center"
+        className="absolute inset-0 flex items-center justify-center
+        bg-background dark:bg-grid-white/[0.2] bg-grid-black/[0.2]
+        [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]
+        z-10"
       >
         {/* Radial gradient for the container to give a faded look */}
         <div
