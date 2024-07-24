c3a5d4b7a183374c6f565ec8207f21b55b6a0bc4
Mark Wang <armstrong.wang2000@gmail.com>
2024-07-24 17:02:25 -0600

    Make MarkdownRender looks more clean.

--- components/common/MarkdownRender.tsx
+++ components/common/MarkdownRender.tsx
@@ -1,5 +1,6 @@
 import "@/components/common/MarkdownRender.css";
-import Markdown from "react-markdown";
+import { ClassAttributes, HTMLAttributes } from "react";
+import Markdown, { ExtraProps } from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import codeStyle from "react-syntax-highlighter/dist/esm/styles/prism/vsc-dark-plus";
 import remarkGfm from "remark-gfm";
@@ -8,36 +9,35 @@ interface MarkdownRendererProps {
   content: string;
 }
 
+const Code = ({
+  children,
+  className,
+  ...rest
+}: ClassAttributes<HTMLElement> & HTMLAttributes<HTMLElement> & ExtraProps) => {
+  const match = /language-(\w+)/.exec(className ?? "");
+  if (!match) {
+    return (
+      <code className={className} {...rest}>
+        {children}
+      </code>
+    );
+  }
+  return (
+    <SyntaxHighlighter
+      PreTag="div"
+      language={match[1]}
+      style={codeStyle}
+      customStyle={{ padding: 0, margin: 0, background: "transparent" }}
+    >
+      {String(children).replace(/\n$/, "")}
+    </SyntaxHighlighter>
+  );
+};
+
 const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ content }) => {
   return (
     <article className="prose dark:prose-invert">
-      <Markdown
-        remarkPlugins={[remarkGfm]}
-        components={{
-          code(props) {
-            const { children, className, node, ...rest } = props;
-            const match = /language-(\w+)/.exec(className || "");
-            return match ? (
-              <SyntaxHighlighter
-                PreTag="div"
-                language={match[1]}
-                style={codeStyle}
-                customStyle={{
-                  padding: "0",
-                  margin: "0",
-                  background: "transparent",
-                }}
-              >
-                {String(children).replace(/\n$/, "")}
-              </SyntaxHighlighter>
-            ) : (
-              <code {...rest} className={className}>
-                {children}
-              </code>
-            );
-          },
-        }}
-      >
+      <Markdown remarkPlugins={[remarkGfm]} components={{ code: Code }}>
         {content}
       </Markdown>
     </article>
