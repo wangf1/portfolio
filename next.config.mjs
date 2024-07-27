9ae140e153d938662be5068808410d71b0a778ff
Mark Wang <armstrong.wang2000@gmail.com>
2024-07-26 18:33:22 -0600

    Redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/about_me",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
