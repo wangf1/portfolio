2f87a732404b785fc82f4e31bf20ba173d983d22
Mark Wang <armstrong.wang2000@gmail.com>
2024-07-29 04:59:12 -0600

    Use markdown as about me and experience, delete unused files.

---
+++ frontend/feature/resume/CoverLetterContent.ts
@@ -0,0 +1,30 @@
+export const CoverLetterContent = `
+# Who Am I?
+
+My name is **Mark Wang**, currently living in **Calgary, Canada**, with permanent resident status.
+
+With **more than 10 years** of experience as a software developer, I have built a solid foundation across various technology stacks.
+
+I am a quick learner, always ready to embrace new technologies and advance my skills.
+
+---
+
+# My Origins and Purpose
+
+I come from **Shanghai**. Experiencing the extreme lockdowns of 2022 gave me a new understanding of freedom and dignity. I spent 30 years from birth to becoming aware of the world, and another 13 years grasping the essence of modern civilization.
+
+I am deeply grateful to the **Canadian people** and government for their commitment to diversity and inclusiveness, which allows me to share and uphold these same values and way of life. I am dedicated to working hard, being self-sufficient, and contributing to the community in return for the opportunity to live here.
+
+I know where I come from, why I am here, and what I should do and enjoy. I will never forget this journey!
+
+---
+
+# My Career Goals
+
+- **Master** full-stack development with a strong emphasis on frontend frameworks like **ReactJS**.
+- **Explore and integrate** diverse tech stacks beyond the Java ecosystem, including TypeScript with Node.js, Python with Django, C# with .NET Core, and Ruby on Rails.
+- **Leverage and enhance** my expertise in Java enterprise development to drive innovation and deliver high-quality solutions.
+- **Expand** my proficiency with leading cloud platforms such as AWS and Azure.
+- **Embrace** relocation opportunities within Canada as needed.
+- As a new immigrant, I am not yet settled in my current city and lack local work experience, so I am **flexible** regarding job location and level.
+`;
---
+++ frontend/feature/resume/ResumeContent.ts
@@ -0,0 +1,112 @@
+export const ResumeContent = `
+# Skills
+
+### Programming Languages
+
+- **Java 8/11/17:** 8+ years
+- **JavaScript:** 5 years
+- **TypeScript:** 2 years
+- **Python:** 2 years
+- **SQL:** 8+ years
+- **Go, C#, Kotlin, Ruby:** Can pick up quickly (within one week)
+- **Rust, C, C++:** Can pick up relatively quickly (within one month)
+
+### Frameworks and Libraries
+
+- **Spring Boot:** (8+ years)
+- **React:** (2 years)
+- **Node.js:** (2 years)
+- **Django:** (1 year)
+- **Next.js:** (1 year)
+- **JPA, Redux, Prisma, Drizzle, Mongoose, CSS, Tailwind, MUI:** 0.5 ~ 2 years
+
+### Tools
+
+- **Git:** (8+ years)
+- **IntelliJ IDEA:** (8+ years)
+- **VS Code:** (5 years)
+- **PyCharm:** (2 years)
+- **BDever, Postman, Studio 3T, etc:** (0.5 ~ 5 years)
+
+### Methodologies, Cloud, Databases, Event Systems, Caching, Testing, Build Tools, Security, Container, CI/CD, etc.
+
+- **Methodologies:** Agile, Scrum, Design Pattern
+- **Cloud Platforms:** SAP Cloud Platform, Cloud Foundry, AWS, Azure
+- **Databases:** SAP HANA, PostgreSQL, MongoDB, MySQL
+- **Event Systems:** Kafka, RabbitMQ
+- **Caching:** Redis
+- **Testing:** Mockito, Mocha, Jest
+- **Build Tools:** Gradle, Maven, npm, yarn, etc.
+- **Security:** OAuth, JWT, Spring Security, NextAuth, Clerk
+- **Container**: Docker
+- **CI/CD:** Jenkins, GitLab
+
+---
+
+# Work Experience
+
+### Expert Software Engineer - SAP SuccessFactors
+
+_June 2019 to May 2024, Shanghai, China_
+
+- **Role:** Lead Developer for SuccessFactors Employee Central, the world’s leading Core HR solution.
+
+- **Key Achievements:**
+
+  - Designed and implemented an event-driven system integrating existing functionalities, which supported **two** new microservices.
+  - Developed APIs for batch jobs, resulting in a **95%** performance improvement.
+  - Refactored complex business logic using design patterns to enhance maintainability and extensibility.
+  - Delivered multiple high-impact features that directly improved the customer experience.
+
+- **Other Responsibilities:** Authored technical documentation, mentored junior developers, and led design and code reviews.
+
+- **Key Technologies:** Java, Spring, Design Patterns, Event-Driven Architecture, Kafka, Redis, Docker, REST, SaaS Cloud
+
+### Senior Software Engineer - SAP
+
+_July 2013 to June 2019, Shanghai, China_
+
+- **Role:** Key Developer for SAP Cloud Platform Mobile Services, a suite of cloud-native microservices.
+
+- **Key Contributions:**
+
+  - Developed platform administration tools using the SAP UI5 JavaScript framework.
+  - Architected and implemented **two** cloud-native microservices with Spring Boot.
+  - Created a Node.js service to aggregate health statuses from multiple microservices.
+  - Refactored a synchronous proxy service into an asynchronous responsibility chain, improving performance by **40%**.
+
+- **Other Responsibilities:** Authored technical documentation, participated in design and code reviews, and engaged in knowledge-sharing sessions.
+
+- **Key Technologies:** Java, Spring Boot, SAP UI5, JavaScript, Node.js, Cloud-Native Architecture
+
+---
+
+# Education
+
+- **Master's Degree** in Software Engineering, Shanghai Jiao Tong University, Shanghai, China, September 2005 - April 2008
+- **Bachelor's Degree** in Electronics and Communications Engineering, Tianjin University, Tianjin, China, September 1999 - June 2003
+
+---
+
+# Links
+
+- <a href="https://www.linkedin.com/in/feng-wang-ba373633" target="_blank" rel="noopener noreferrer" className="text-blue-500 dark:text-blue-300 hover:underline flex items-center w-fit">LinkedIn Profile
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+    class="lucide lucide-square-arrow-out-up-right">
+    <path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6" />
+    <path d="m21 3-9 9" />
+    <path d="M15 3h6v6" />
+</svg></a>
+
+- <a href="https://github.com/wangf1" target="_blank" rel="noopener noreferrer" className="text-blue-500 dark:text-blue-300 hover:underline flex items-center w-fit">GitHub Profile
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+    class="lucide lucide-square-arrow-out-up-right">
+    <path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6" />
+    <path d="m21 3-9 9" />
+    <path d="M15 3h6v6" />
+</svg></a>
+`;
