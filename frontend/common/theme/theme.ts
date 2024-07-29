6a46e290056ea275fb15ddd19b083aec9a5f0407
Mark Wang <armstrong.wang2000@gmail.com>
2024-07-28 20:12:03 -0600

    The default text.secondary color in MUI's light mode is too light.

--- frontend/common/theme/theme.ts
+++ frontend/common/theme/theme.ts
@@ -4,6 +4,9 @@ import { Theme, createTheme } from "@mui/material/styles";
 export const lightTheme: Theme = createTheme({
   palette: {
     mode: "light",
+    text: {
+      secondary: "#4b5563", // text-gray-600
+    },
   },
 });
 
