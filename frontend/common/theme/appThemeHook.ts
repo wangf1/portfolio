0fbb1b5b8536f3a19f158daee32be9bd6381cbb0
Mark Wang <armstrong.wang2000@gmail.com>
2024-07-28 19:55:59 -0600

    If not local storage, use dary by default, regardless system theme. Otherwise if user change theme, respect user's preference. Which means, if local storage has valid theme value, repect it.  And if user change system theme, will change theme accordingly.

--- frontend/common/theme/appThemeHook.ts
+++ frontend/common/theme/appThemeHook.ts
@@ -15,7 +15,10 @@ type ThemeString = "light" | "dark";
  * @return An array containing the current theme and a function to update the theme.
  */
 const useAppTheme = (): ["light" | "dark", (e: ThemeString) => void] => {
-  const [theme, setTheme] = useState<ThemeString>("light");
+  const themeInLocalStorage = localStorage.getItem("theme");
+  //If no theme in local storage, or value is not light, then default to dark.
+  const currentTheme = themeInLocalStorage === "light" ? "light" : "dark";
+  const [theme, setTheme] = useState<ThemeString>(currentTheme);
 
   useEffect(() => {
     const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
@@ -24,16 +27,6 @@ const useAppTheme = (): ["light" | "dark", (e: ThemeString) => void] => {
     };
 
     mediaQuery.addEventListener("change", handleChange);
-
-    const themeInLocalStorage = localStorage.getItem("theme");
-    let currentTheme: ThemeString;
-    if (themeInLocalStorage === "dark" || themeInLocalStorage === "light") {
-      currentTheme = themeInLocalStorage;
-    } else {
-      currentTheme = mediaQuery.matches ? "dark" : "light";
-    }
-
-    setTheme(currentTheme);
 
     return () => mediaQuery.removeEventListener("change", handleChange);
   }, []);
